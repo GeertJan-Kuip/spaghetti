ecf3ff46113db71935e1a9d2f4544b1562a566de
GeertJan-Kuip <geertjan679@gmail.com>
2025-01-31 22:10:34 +0100

    Slight change in TextFileReader class. - Lists replaced by HashSets - '@' (unicode 64) added to HashSet variable wordsAndNumbers.

--- src/main/java/com/geertjankuip/texthandling/TextFileReader.java
+++ src/main/java/com/geertjankuip/texthandling/TextFileReader.java
@@ -6,9 +6,7 @@ import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 
 public class TextFileReader {
@@ -22,11 +20,11 @@ public class TextFileReader {
     private ArrayList<Integer> positions = new ArrayList<Integer>();
 
     // simpleOperator  - (),.;[]{} non-ambiguous, always one character
-    final private List<Integer> simpleOperator = Arrays.asList(40, 41, 44, 46, 59, 91, 93, 123, 125);
+    final private Set<Integer> simpleOperator = new HashSet<>(Arrays.asList(40, 41, 44, 46, 59, 91, 93, 123, 125));
     // complexOperator: !%&*+-/:<=>^|    -  these operators need next character before they can be interpretated
-    final private List<Integer> complexOperator = Arrays.asList(33, 37, 38, 42, 43, 45, 47, 58, 60, 61, 62, 94, 124);
-    // wordStart: A-Z,a-z,1-9,_,$
-    final private List<Integer> wordsAndNumbers = Arrays.asList(36, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 95, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122);
+    final private Set<Integer> complexOperator = new HashSet<>(Arrays.asList(33, 37, 38, 42, 43, 45, 47, 58, 60, 61, 62, 94, 124));
+    // wordStart: A-Z,a-z,1-9,_,$,@
+    final private Set<Integer> wordsAndNumbers = new HashSet<>(Arrays.asList(36, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 95, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122));
 
 
     public TextFileReader(String path, ActivityLogger logger) {
